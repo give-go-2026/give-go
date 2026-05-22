c39320e731eef0d2056c4fe0bc3b9d5992b2f160
Kovács Zsombor <125221225+KoZsombat@users.noreply.github.com>
2026-05-22 13:02:27 +0200

    build fix

--- apps/web/src/app/auth/register/page.tsx
+++ apps/web/src/app/auth/register/page.tsx
@@ -27,7 +27,7 @@ export default async function RegisterPage({
     return (
       <AuthPage>
         <AuthCard>
-          <RegistrationSuccess type={type} />
+          <RegistrationSuccess type={type as RegistrationType} />
         </AuthCard>
       </AuthPage>
     );
